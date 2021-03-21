ddb0ee3433c6021b0b54e114eefb066b8e964f39
Bluette1 <marylene.sawyer@gmail.com>
2021-03-21 13:28:36 +0200

    Add progressbar

---
+++ Progressbar.js
@@ -0,0 +1,28 @@
+const progressBarContainer = document.querySelector('.progressbar-main');
+const progressBar = document.createElement('div');
+progressBarContainer.append(progressBar);
+progressBar.classList.add('progressbar');
+
+const RATES = [0, 100, 200, 300, 400];
+let start = true;
+let idx = 0;
+
+const showProgress = () => {
+	 
+	if (start) {
+		start = false;
+		const id = setInterval(shift, 1000);
+		function shift() {
+			if (idx > RATES.length - 1) {
+				clearInterval(id);
+				start = true;
+				idx = 0;
+				showProgress();
+			} else {
+				progressBar.style.width = `${(RATES[idx] / RATES[RATES.length - 1]) * 100}%`;
+				idx += 1;
+			}
+		}
+    }
+}
+showProgress();

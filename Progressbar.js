2ffaa4c6396b8359028e4ce8770cc15c8dc82f9f
Bluette1 <marylene.sawyer@gmail.com>
2021-03-22 08:17:14 +0200

    Move function declaration to function body root

--- Progressbar.js
+++ Progressbar.js
@@ -7,21 +7,27 @@ const RATES = [0, 100, 200, 300, 400];
 let start = true;
 let idx = 0;
 
+let id;
+
+const shift = () => {
+  if (idx > RATES.length - 1) {
+    clearInterval(id);
+    start = true;
+    idx = 0;
+  } else {
+    progressBar.style.width = `${(RATES[idx] / RATES[RATES.length - 1]) * 100}%`;
+    idx += 1;
+  }
+};
+const updateId = () => {
+  id = setInterval(shift, 1000);
+};
 const showProgress = () => {
   if (start) {
     start = false;
-    const id = setInterval(shift, 1000);
-    function shift() {
-      if (idx > RATES.length - 1) {
-        clearInterval(id);
-        start = true;
-        idx = 0;
-        showProgress();
-      } else {
-        progressBar.style.width = `${(RATES[idx] / RATES[RATES.length - 1]) * 100}%`;
-        idx += 1;
-      }
-    }
+    updateId();
   }
 };
+
 showProgress();
+setInterval(showProgress, 3500);
